618f5376930860ec777bd2d490788b59cdaf393f
Sarthak Mishra <31609472+sarthakmis@users.noreply.github.com>
2019-09-13 15:36:49 +0530

    Update base.js

--- utils/base.js
+++ utils/base.js
@@ -1,9 +1,4 @@
-/*
- * @Author: Anooj Krishnan G 
- * @Date: 2019-05-05 15:33:52 
- * @Last Modified by: Anooj Krishnan G
- * @Last Modified time: 2019-05-28 00:51:49
- */
+
 import strings from './strings';
 import colors from './colors';
 import images from './images';
